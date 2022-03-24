5b0d935d9ebaa422bc2c03cba2caaabd2db5c097
Nicolas <wellcomenicolas@gmail.com>
2022-03-24 16:19:08 +0100

    Add scount

--- asset/js/main.js
+++ asset/js/main.js
@@ -7,3 +7,39 @@ L'output del prezzo finale va scritto in forma umana (con massimo due decimali, 
  */
 
 
+// Chiedi all'utente il numero dei KM da percorrere
+const numero_km = parseInt(prompt("Quanti KM dovrai percorrere?"));
+console.log("Il numero dei KM da percorrere è: " + numero_km);
+
+if (isNaN(numero_km)) {
+    alert("Devi inserire un valore numerico");
+}
+
+// Chiedi all'utente l'età 
+
+const eta_utente = parseInt(prompt("Quanti anni hai?"));
+console.log("L'età dell'utente è: " + eta_utente);
+
+if (isNaN(eta_utente)) {
+    alert("Devi inserire un valore numerico");
+}
+
+//Calcoliamo il costo del biglietto al netto degli sconti
+
+const prezzo_totale = numero_km * 0.20;
+let prezzo_sconto;
+console.log("Il prezzo totale senza sconti è: " + prezzo_totale + " €");
+
+//Calcoliamo il costo del biglietto applicando lo sconto
+
+if (eta_utente < 18) {
+    prezzo_sconto = (prezzo_totale - ((prezzo_totale * 20) / 100));
+} else if (eta_utente >= 65) {
+    prezzo_sconto = (prezzo_totale - ((prezzo_totale * 40) / 100));
+} else {
+    prezzo_sconto = prezzo_totale;
+}
+
+console.log("Il prezzo scontato è: " + prezzo_sconto + " €");
+
+document.getElementById("prezzo").innerHTML = prezzo_sconto.toFixed(2) + " €";
